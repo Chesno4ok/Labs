370c801abf83495f363bc9b4b21161fd7b26a6c0
Chesnok <d.chesnokov20@gmail.com>
2022-11-09 11:06:21 +0300

    -

--- LABS2.cpp
+++ LABS2.cpp
@@ -3,100 +3,34 @@
 #include <string.h>
 #include <Windows.h>
 
+int revert(int a)
+{
+	int n = a;
+
+	string temp = "";
+
+	string t1 = to_string(n);
+
+	for (int i = t1.length() - 1; i >= 0; i--)
+		temp += t1[i];
+
+	n = stoi(temp);
+
+	return n;
+}
+
 using namespace std;
 int main()
 {
 	SetConsoleCP(1251);
 	SetConsoleOutputCP(1251);
 
-	cout << "dsfdsfd" << endl;
-	string t = "Собака кошка бегемот ";
-	string gl = "аоуэыяёюеиАОУЭЫЯЁЮЕИ";
+	cout << revert(12);
+	
+	cout << "Hello world \n ";
 
-	string temp = "";
-	string temp1;
+	
 
-	string* qs = new string[t.length()];
-	int qsc = 0;
-
-	int c = 0;
-	int q = 0;
-	int st = 0, nd = 0;
-	bool IsChecking = false;
-
-	for (int i = 0; i < t.length(); i++)
-	{
-
-		if (IsChecking)
-		{
-			if (t[i] == *" ")
-			{
-				IsChecking = false;
-				nd = i;
-
-			}
-			
-			if (!IsChecking)
-			{
-
-				temp.append(t, st, nd);
-				c = 0;
-				for (int x = 0; x < temp.length(); x++)
-				{
-					temp1 = temp[x];
-
-					if (temp1.find_first_of(gl) == 0)
-					{
-						c++;
-					}
-				}
-				if (c > q)
-				{
-					for (int x = 0; x < t.length(); x++)
-					{
-						qs[x] = " ";
-					}
-
-					q = c;
-					qsc = 0;
-					qs[qsc] = temp;
-
-					IsChecking = false;
-					temp = "";
-				}
-				else if (c == q)
-				{
-					qsc++;
-					qs[qsc] = temp;
-
-					IsChecking = false;
-					temp = "";
-				}
-				else
-				{
-					IsChecking = false;
-					temp = "";
-				}
-
-			}
-		}
-		else
-		{
-			if ((t[i] >= *"а" || t[i] <= *"я") || (t[i] >= *"А" || t[i] <= *"Я"))
-			{
-				st = i;
-				nd = 0;
-				IsChecking = true;
-			}
-		}
-
-	}
-
-
-	cout << endl;
-	for (int i = 0; i < t.length(); i++)
-	{
-		cout << qs[i] << " ";
-	}
+	
 
 }
